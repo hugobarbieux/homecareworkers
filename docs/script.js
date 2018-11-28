6854bbcf36dec090f8a11d8eed8f6611e8d9486c
hugobarbieux <43614788+hugobarbieux@users.noreply.github.com>
2018-11-28 20:23:47 +0000

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,10 @@
+var $ = jQuery.noConflict();
+	
+	$(document).ready(function() {
+		$('.scrollTo').click( function() { 
+			var page = $(this).attr('href'); 
+			var speed = 750; 
+			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); 
+			return false;
+		});
+	});
